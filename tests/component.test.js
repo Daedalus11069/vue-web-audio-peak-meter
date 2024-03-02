7ec93ebb8593bbae7abbd5fcbf769faed1a02e61
daedalus11069 <daedalus969@gmail.com>
2024-03-02 13:48:59 -0800

    Fix testing

--- tests/component.test.js
+++ tests/component.test.js
@@ -3,9 +3,14 @@ import { mount } from '@vue/test-utils';
 import { WebAudioPeakMeter } from '../src/components';
 
 describe('component is able to mount', () => {
-  it('is true', () => {
+  it('can render the ticks', () => {
     const wrapper = mount(WebAudioPeakMeter, { props: { srcNode: null } });
 
     expect(wrapper.text()).toContain('-42');
+  });
+  it('can render the ticks at the correct offset', () => {
+    const wrapper = mount(WebAudioPeakMeter, { props: { srcNode: null } });
+
+    expect(wrapper.findAll('.tick').at(4).attributes().style).toBe('--percent-in-range: 37.5%;');
   });
 });
